8fb2733364b4c5111d312c03f8608fa7bff478e0
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-03-12 08:38:57 -0700

    SetUp LARAVEL Project

---
+++ traking-app/app/Models/Demande.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Demande extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["id","nom","email_pro","nom_De_Societe","Phone_Number"];
+}
