37a8ec276ea9e2398d5ac908f1fa887beb48e565
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-04-08 08:34:29 -0700

    Update AuthController.php

--- app/Http/Controllers/APIs/AuthController.php
+++ app/Http/Controllers/APIs/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -57,4 +58,56 @@ class AuthController extends Controller
             'message'=>'User Logged Out Successfully'
         ]);
     }
+
+
+    public function update_Image_Profile(Request $request)
+    {
+        try {
+            // Validate the incoming request
+            $validateUser = $request->validate([
+                "image" => "required|string",
+                "matricule"=>"required|string"
+            ]);
+
+            // Retrieve the base64 encoded image data from the request
+            $imageData = $request->input("image");
+            $matricule = $request->input("matricule");
+
+            // Decode the base64 string
+            $decodedImageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imageData));
+
+            // Generate a unique filename
+            $imageName = "image_" . time() . ".png";
+
+            $user = User::where("Matricule",$matricule);
+
+            // Save the image to the "Images" folder within the "public" directory
+            file_put_contents(public_path("Images/" . $imageName), $decodedImageData);
+
+
+            // Generate the image URL
+            $imageUrl = asset("Images/" . $imageName);
+
+            $affected = DB::table('users')
+                    ->where('matricule', $matricule)
+                    ->update(['Image' => $imageUrl]);
+
+        if ($affected == 0) {
+            throw new \Exception("User not found with matricule: " . $matricule);
+        }
+
+            // Return a JSON response indicating successful image upload along with the image URL
+            return response()->json([
+                "message" => "Image uploaded successfully",
+                "imageUrl" => $imageUrl
+            ]);
+        } catch (\Exception $e) {
+            // Return a JSON response with an error message if an exception occurs
+            return response()->json([
+                "error" => "Image upload failed: " . $e->getMessage()
+            ], 500);
+        }
+    }
+
 }
+
